0c98aeab27f810ad291b34d07cf8e0e82810a8ec
Jason Nutt <jnutt367@gmail.com>
2021-04-11 17:24:09 -0400

    commit new animation for guy further down

--- gsap.js
+++ gsap.js
@@ -1,4 +1,4 @@
-gsap.fromTo(".first", { x: 1, opacity: "0" }, { duration: 6, rotate: 360, scale: 2, ease: "back", borderRadius: "50%", y: -10, border: "4px inset black", opacity: "1" });
+gsap.fromTo(".first", { x: 1, opacity: "0" }, { duration: 6, rotate: 360, scale: 2, ease: "back", borderRadius: "50%", y: 200, border: "4px inset black", opacity: "1" });
 gsap.fromTo(".stars", { x: 1, opacity: "0" }, { duration: 6, rotate: 360, scale: 2, borderRadius: "50%", y: -10, opacity: "1" });
 gsap.to(".second", { duration: 2, y: 200 });
 
